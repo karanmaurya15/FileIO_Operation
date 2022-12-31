eb17ed1688e7eb1699f0bc2375fe60795175339e
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-31 23:36:46 +0530

    Initial comit

---
+++ FileIO_Operation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIO_Operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to File I/O Operation");
+        }
+    }
+}
